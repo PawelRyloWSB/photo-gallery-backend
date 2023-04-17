a4aa8722bd886d231ceb10f3784c8c9aaaa3d5d8
rylcior <rylcior@gmail.com>
2023-04-17 18:38:02 +0200

    Add auth module and database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { databaseConfig } from './config/database.config';
+import { ConfigModule } from '@nestjs/config';
+import { envValidationSchema } from './config/envValidation.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+    TypeOrmModule.forRootAsync(databaseConfig),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env`],
+      validationSchema: envValidationSchema,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
